113cfc424aa27a6dd59983bbd921e98e2f12cc14
DESKTOP-Q3ASJ93\ReaL <Real1107055@gmail.com>
2017-02-09 02:51:25 -0600

    Dialog created for taking user name

--- app/src/main/java/com/example/real/mychatapp/MainActivity.java
+++ app/src/main/java/com/example/real/mychatapp/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.real.mychatapp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etRoomName;
+    Button btnAddRoom;
+    ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_rooms = new ArrayList<>();
+    private String name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etRoomName = (EditText) findViewById(R.id.etRoomName);
+        btnAddRoom = (Button) findViewById(R.id.btn_add_room);
+        listView = (ListView) findViewById(R.id.listview);
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list_of_rooms);
+        listView.setAdapter(arrayAdapter);
+
+        requestUserName();
+    }
+
+    private void requestUserName() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter your name: ");
+
+        final EditText inputField = new EditText(this);
+        builder.setView(inputField);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                name = inputField.getText().toString();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                requestUserName();
+            }
+        });
+
+        builder.show();
     }
 }
